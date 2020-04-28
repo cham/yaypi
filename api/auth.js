7213ce4f07608b6eecfc6e9e96203b57e6620718
Dan <daniel@sibitzky.com>
2020-04-28 12:52:21 -0400

    simple registration

--- api/auth.js
+++ api/auth.js
@@ -5,6 +5,27 @@ const config = require('../config')
 
 const JWT_SECRET = config.get('JWT_SECRET')
 
+
+const register = ({ username, password, email }) =>
+  new Promise((resolve, reject) => {
+    const saltRounds = 10
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      if (err) {
+        return reject(err)
+      }
+      db.Users.create({
+        username,
+        email,
+        password: hash,
+        created: new Date(),
+        urlname: '',
+        last_ip: '',
+        custom_css: '',
+        custom_js: '',
+      }).then(resolve)
+    })
+  })
+
 const login = ({ username, password }) => db.Users.findOne({ username }, { password: 1 })
   .then(userDoc => new Promise((resolve, reject) => bcrypt.compare(password, userDoc.password, (err, passwordMatch) => {
     if (err) {
@@ -24,6 +45,7 @@ const verifyToken = ({ token }) => new Promise((resolve, reject) => jwt.verify(t
 }))
 
 
+exports.register = register
 exports.login = login
 exports.getToken = getToken
 exports.verifyToken = verifyToken
